7030c60ebb25bf08dd499e81f950cf2668d4ac06
kobi <chiadingobili@yahoo.com>
2017-03-22 09:27:43 -0500

    updated table second

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'home#index'
-
   resources :package_requests
   resources :zips
   resources :countries
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313232826) do
+ActiveRecord::Schema.define(version: 20170320231211) do
 
   create_table "buildings", force: :cascade do |t|
     t.string   "building_description"
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(version: 20170313232826) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
