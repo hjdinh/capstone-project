05681084e0d2abd5ad49f50cd9142c774472406f
Dwayne Belcon <belcon91@hotmail.com>
2017-04-03 11:34:22 -0500

    Dwayne & Jimmy Merged

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,40 +1,10 @@
 class Item < ActiveRecord::Base
   has_many :loan_histories
-<<<<<<< HEAD
-<<<<<<< HEAD
+
   belongs_to :form
   belongs_to :building
   belongs_to :tag_status
   belongs_to :vendor
   belongs_to :item_status
-<<<<<<< HEAD
-  validates_presence_of :vendor_id
-  validates_presence_of :item_status_id
-  validates_presence_of :tag_status_id
-=======
-  has_many :package_requests
-=======
-  belongs_to :package_request
->>>>>>> a511f83882486e6fc6f4ba544c62fb3938b3ae06
-  belongs_to :building
-  belongs_to :tag_type
-  belongs_to :vendor
-  belongs_to :item_status
-<<<<<<< HEAD
-  belongs_to :item_category
-  validates_presence_of :vendor_id
-  validates_presence_of :item_status_id
-  validates_presence_of :tag_type_id
->>>>>>> 7ed43a1aaee4e6343ead889d21b385df83919228
-=======
-  validates_presence_of :vendor_id
-  validates_presence_of :item_status_id
-  validates_presence_of :tag_type_id
->>>>>>> a511f83882486e6fc6f4ba544c62fb3938b3ae06
-  validates_presence_of :building_id
-  validates_presence_of :serial_number
-  validates_presence_of :item_name
-=======
 
->>>>>>> 4258dfe8faef7f48f35e88d9bc2e049ef3cfb561
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,19 +1,3 @@
 class Vendor < ActiveRecord::Base
   has_many :items
-  validates_presence_of :vendor_name
-<<<<<<< HEAD
-  validates_presence_of :email
-<<<<<<< HEAD
-<<<<<<< HEAD
-  validates_presence_of :city
-  validates_presence_of :state
-  validates_presence_of :country
-  validates_presence_of :phone_number
-  validates_presence_of :zip
-=======
->>>>>>> 7ed43a1aaee4e6343ead889d21b385df83919228
-=======
->>>>>>> a511f83882486e6fc6f4ba544c62fb3938b3ae06
-=======
->>>>>>> 4258dfe8faef7f48f35e88d9bc2e049ef3cfb561
 end
