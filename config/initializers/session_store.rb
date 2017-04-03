75a47de6abae2dd2c64e7e7772edf30ecf6c13fe
Dwayne Belcon <belcon91@hotmail.com>
2017-04-03 11:48:06 -0500

    Dwayne & Jimmy Merged

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,4 @@
 # Be sure to restart your server when you modify this file.
-
-<<<<<<< HEAD
-<<<<<<< HEAD
 Rails.application.config.session_store :cookie_store, key: '_Concave_project_session'
-=======
-Rails.application.config.session_store :cookie_store, key: '_Updated_project_session'
->>>>>>> 7ed43a1aaee4e6343ead889d21b385df83919228
-=======
-Rails.application.config.session_store :cookie_store, key: '_Updated_project_session'
->>>>>>> a511f83882486e6fc6f4ba544c62fb3938b3ae06
+
+
