0d787b2869d8bb6de0fa34625cf52516b3a9199a
Dwayne Belcon <belcon91@hotmail.com>
2017-04-03 21:57:46 -0500

    Fixed Navbar Links

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   resources :order_requests
   resources :package_requests
   resources :home
+
+
   devise_for :users, controllers: {registrations: "registrations"}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
