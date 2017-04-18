5b6700ea2059384194a9fe01095dadc778dbaf31
Evo (Dwayne) <belcon91@hotmail.com>
2017-04-17 21:47:28 -0500

    PostgreSQL Setup

--- db/migrate/20170322160304_change_column_building_number_in_buildings.rb
+++ db/migrate/20170322160304_change_column_building_number_in_buildings.rb
@@ -1,5 +1,5 @@
 class ChangeColumnBuildingNumberInBuildings < ActiveRecord::Migration
   def change
-    change_column :buildings, :building_number, :integer
+    change_column :buildings, :building_number, 'integer USING CAST(building_number AS integer)'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170410185244) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "buildings", force: :cascade do |t|
     t.string   "building_description"
     t.integer  "building_number"
@@ -130,8 +133,8 @@ ActiveRecord::Schema.define(version: 20170410185244) do
     t.boolean  "admin"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "vendors", force: :cascade do |t|
     t.string   "vendor_name"
@@ -152,4 +155,5 @@ ActiveRecord::Schema.define(version: 20170410185244) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "item_locations", "users"
 end
